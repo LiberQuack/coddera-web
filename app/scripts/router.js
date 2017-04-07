79944952daeecb0276640233e5f257910c6890a4
ThiagoMartins <rogue.thiago@gmail.com>
2017-04-07 17:03:51 -0300

    Implemented router to load views

--- app/scripts/router.js
+++ app/scripts/router.js
@@ -1,14 +1,34 @@
 'use strict';
 
 (function () {
-  function route(path) {
 
+  function route(path) {
     switch(true) {
       case (path === '/'):
-        console.log("On Home Page");
-    }
+        loadView("/views/landing.html");
+        break;
 
+      case (path === '/users'):
+        console.log('On Users Listage');
+        break;
+
+      case (path === '/users/new'):
+        console.log('On Users Listage');
+        break;
+
+      case (/\/users\/.+/.test(path)):
+        console.log('On User X');
+        break;
+    }
   }
 
-  window.addEventListener('hashchange');
+  function loadView(url) {
+    fetch(url)
+      .then(res => res.text())
+      .then(htmlText => document.querySelector(".js-route").innerHTML = htmlText);
+  }
+
+  //Init
+  route(location.hash.replace(/^#/, ''));
+  window.addEventListener('hashchange', e => route(location.hash.replace(/^#/, '')));
 })();
